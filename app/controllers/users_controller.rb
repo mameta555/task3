89b5ec45b83dd99ace089f2c56592a98c94a4466
EC2 Default User <ec2-user@ip-172-31-15-158.ap-northeast-1.compute.internal>
2022-11-26 07:54:12 +0000

    [add]ユーザー編集更新機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def index
     @users = User.all
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
